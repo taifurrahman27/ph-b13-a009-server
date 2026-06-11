7e9a4c7ea064c4046db3bede6f2cffb22bce330c
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-06-12 00:22:50 +0600

    Mongodb database connection stablished

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dontenv = require("dotenv");
+dontenv.config();
+
 
 app.get('/', (req, res) => {
     res.send('Studynook server is running fine on PORT')
 })
+
+
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
